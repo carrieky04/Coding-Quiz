7810c5adbcac624dfece2c65f39dadc17d073516
Carrie Young <carrieky04@gmail.com>
2022-10-20 22:18:08 -0700

    adds more js, deletes some excess html

--- script.js
+++ script.js
@@ -1,50 +1,45 @@
-const startBtn = document.getElementById("start-btn");
-const options = Array.from(document.getElementById("option-buttons"));
+var startBtn = document.getElementById("start-btn");
 var firstQuestion = document.querySelector (".startquiz");
 var questionContainer = document.querySelector("#hide");
 var shownQuestion = document.querySelector(".container");
-
-
+var questionHeading = document.querySelector("#question");
+var optionsContainer = document.querySelector("#option-buttons");
 
 const questions = [
     {
         question: "What is 2+2?",
-        option1: "4",
-        option2: "5",
-        option3: "6",
-        option4: "7",
-        answer: "1",
+        options: ["4","5", "6", "7"],
+        answer: 0,
     },
     {
         question: "What is 3+2?",
-        option1: "4",
-        option2: "5",
-        option3: "6",
-        option4: "7",
-        answer: "2",
+        options: ["4","5", "6", "7"],
+        answer: 1,
     },
     {
         question: "What is 4+2?",
-        option1: "4",
-        option2: "5",
-        option3: "6",
-        option4: "7",
-        answer: "3",
+        options: ["4","5", "6", "7"],
+        answer: 2,
     },
     {
         question: "What is 5+2?",
-        option1: "4",
-        option2: "5",
-        option3: "6",
-        option4: "7",
-        answer: "4",
+        options: ["4","5", "6", "7"],
+        answer: 3,
     },
 ]
 
 
 
 startBtn.addEventListener("click", startQuiz);
-
+console.log(questions[0].question);
+questionHeading.textContent = questions[0].question
+for (let i = 0; i < questions[0].options.length; i++) {
+    console.log(questions[0].options[i]);
+    var optionBtn = document.createElement("button");
+    console.log(optionBtn);
+    optionBtn.innerHTML = questions[0].options[i];
+    optionsContainer.appendChild(optionBtn);
+  }
 function startQuiz() {
     // alert('hi');  
     firstQuestion.style.display = 'none';
@@ -55,7 +50,7 @@ function startQuiz() {
 function nextQuestion() {
     questionContainer.style.display = 'flex';
     shownQuestion = questions[Math.floor(Math.random() * questions.length)];
-
+    
 
 }
     
