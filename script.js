3e722e5150902d3190ee8a75e075c5b9d0c8ba64
Carrie Young <carrieky04@gmail.com>
2022-10-22 14:15:58 -0700

    adds possible evevents and event listeners

--- script.js
+++ script.js
@@ -2,29 +2,38 @@ var startBtn = document.getElementById("start-btn");
 var firstQuestion = document.querySelector (".startquiz");
 var questionContainer = document.querySelector("#hide");
 var shownQuestion = document.querySelector(".container");
-var questionHeading = document.querySelector("#question");
-var optionsContainer = document.querySelector("#option-buttons");
+var questionHeading = document.querySelector(".question");
+var optionsContainer = document.querySelector(".option-buttons");
+
+var score = 0;
+var timer = 75;
+
 
 const questions = [
     {
-        question: "What is 2+2?",
-        options: ["4","5", "6", "7"],
+        question: "A variable with a ____ scope is visible everywhere.",
+        options: ["global","local", "international", "regional"],
         answer: 0,
     },
     {
-        question: "What is 3+2?",
-        options: ["4","5", "6", "7"],
+        question: "To get a value back from a function, use the ___ statement",
+        options: ["if/else","return", "for", "invoking"],
         answer: 1,
     },
     {
-        question: "What is 4+2?",
-        options: ["4","5", "6", "7"],
+        question: "The index of the first item in an array is ___.",
+        options: ["1","undefined", "0", "first"],
         answer: 2,
     },
     {
-        question: "What is 5+2?",
-        options: ["4","5", "6", "7"],
+        question: "To access an array item we use it's index wrapped in ___.",
+        options: ["curly brackets","parenthesis", "forward slashes", "square brackets"],
         answer: 3,
+    },
+    {
+        question: "The _____ is the brower's internal representation of your web page.",
+        options: ["CSS","DOM","HTML","JavaScript"],
+        answer: 1,
     },
 ]
 
@@ -40,18 +49,40 @@ for (let i = 0; i < questions[0].options.length; i++) {
     optionBtn.innerHTML = questions[0].options[i];
     optionsContainer.appendChild(optionBtn);
   }
+
+
+
 function startQuiz() {
     // alert('hi');  
     firstQuestion.style.display = 'none';
-    nextQuestion();  
+    start timer
+    score = 0
 }
 
+function setTimer () {
+    decrease timer if answer wrong
 
-function nextQuestion() {
+}
+
+optionBtn.addEventListener("click", setNextQuestion)
+
+function setNextQuestion() {
     questionContainer.style.display = 'flex';
-    shownQuestion = questions[Math.floor(Math.random() * questions.length)];
-    
-
+    if answer correct add to score
+    save score to localStorage
 }
-    
+
+function (quizOver) {
+    show user score
+    save initials
+    submit button
+    show high scores
+}
+
+function (showHighScores) {
+    display high scores
+    go back button
+    clear high scores button
+}
+
 
