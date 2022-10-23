f10430ddc7c6222326433f36dcd68987d1cd6565
Carrie Young <carrieky04@gmail.com>
2022-10-23 02:59:18 -0700

    adds more css, cadds some working js code' git status

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ var shownQuestion = document.querySelector(".container");
 var questionHeading = document.querySelector(".question");
 var optionsContainer = document.querySelector(".option-buttons");
 var timerEl = document.querySelector(".timer");
+// var textArea = document.querySelector(".show-results");
 
-
+var currentQuestion = {};
+var maxQuestions = 5;
+var acceptedAnswers = true;
+// var chosenQuestion = questions[Math.floor(Math.random() * questions.length)];
 var score = 0;
 var timer;
 var timerCount = 75;
@@ -38,15 +42,12 @@ const questions = [
     {
         question: "The _____ is the brower's internal representation of your web page.",
         options: ["CSS","DOM","HTML","JavaScript"],
-        answer: 1,
-    },
+        answer: 4,
+    }
 ]
 
 
 
-// function init () {
-//     showHighScores();
-// }
 
 
 startBtn.addEventListener("click", startQuiz);
@@ -56,62 +57,66 @@ startBtn.addEventListener("click", startQuiz);
 function startQuiz() {
     welcomeScreen.style.display = 'none';
     questionContainer.style.display = 'flex';
+    availableQuestions = [...questions];
     startTimer();
     askQuestion();
-    // setNextQuestion();
+   
 }
 
 function startTimer () {
     timer = setInterval (function() {
         timerCount--;
-        timerEl.innerHTML = timerCount;
+        timerEl.innerHTML = "Time left: " + timerCount;
     }, 1000);
 }
 
-function askQuestion () {
-    questionHeading.textContent = questions[0].question
-    for (let i = 0; i < questions[0].options.length; i++) {
-        console.log(questions[0].options[i]);
-    var optionBtn = document.createElement("button");
-        console.log(optionBtn);
-    optionBtn.innerHTML = questions[0].options[i];
-    optionsContainer.appendChild(optionBtn);
-    }  
-}
-
-
-
-function answerQuestion () {
-    
-   
-}
-
-// optionBtn.addEventListener("click", setNextQuestion)
-
-// function setNextQuestion() {
-//     questionHeading.textContent = questions[1].question
-//     for (let i = 0; i < questions[1].options.length; i++) {
+// function askQuestion () {
+//     questionHeading.textContent = questions[0].question
+//     for (let i = 0; i < questions[0].options.length; i++) {
 //         console.log(questions[0].options[i]);
 //     var optionBtn = document.createElement("button");
 //         console.log(optionBtn);
-//     optionBtn.innerHTML = questions[1].options[i];
-//     optionsContainer.appendChild(optionBtn);
-//     }  
-    // if answer correct add to score
-    // save score to localStorage
+//     optionBtn.innerHTML = questions[0].options[i];
+//     optionsContainer.appendChild(optionBtn); 
+//     optionBtn.setAttribute("style", "color:black; background: purple; margin-bottom: 10px; border-radius: 10px; padding: 5px;") ;
+//     optionBtn.addEventListener("click",nextQuestion);
+//     };  
+// } 
+    
+function askQuestion () { 
+    var questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    questionHeading.textContent = currentQuestion.question
+
+    for (let i = 0; i < currentQuestion.options.length; i++) {
+        console.log(currentQuestion.options[i]);
+    var optionBtn = document.createElement("button");
+        console.log(optionBtn);
+    optionBtn.innerHTML = currentQuestion.options[i];
+    optionsContainer.appendChild(optionBtn); 
+    optionBtn.setAttribute("style", "color:black; background: purple; margin-bottom: 10px; border-radius: 10px; padding: 5px;") ;
+    optionBtn.addEventListener("click",setAnswer);
+    };
+   
+}
+    
+var optionBtn = document.createElement("button");
+
+function setAnswer() {
+   
+    console.log("You did it!");
+        if (optionBtn === currentQuestion.answer) {
+        score++;
+        console.log("your score is: " + score);
+        }
+    }
+    
 
 
-// function (quizOver) {
-//     show user score
-//     save initials
-//     submit button
-//     show high scores
-// }
 
-// function (showHighScores) {
-//     display high scores
-//     go back button
-//     clear high scores button
-// }
 
-// init();
+
+
+
+
+    
